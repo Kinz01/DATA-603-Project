4d7887500992f1c09e4e9be54d52d2364adb8359
frontdevguy <frontdevguy@gmail.com>
2025-03-18 21:52:53 -0600

    add notebook

--- schemas/municipalities_rent.sql
+++ schemas/municipalities_rent.sql
@@ -2,7 +2,7 @@ CREATE TABLE `municipalities_rent` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `municipality` VARCHAR(200) NOT NULL,
     `year` YEAR NOT NULL,
-    `rental_type` ENUM('2 - bedroom', '3 - bedroom', 'bachelor', '') NOT NULL,
+    `rental_type` ENUM('2 - bedroom', '3 - bedroom', 'bachelor', '1 - bedroom') NOT NULL,
     `value` FLOAT NOT NULL,
     PRIMARY KEY (`id`)
 );
